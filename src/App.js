d825ff3ce5782c77d3703ead1bf1a9aedb00afbe
HameemH <hameemhossain2@gmail.com>
2022-03-26 12:37:32 +0600

    testing readme file edit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Bookshop from './Component/Bookshop/Bookshop';
+import QNA from './Component/QNA/QNA.js';
 
 function App() {
   return (
     <div >
       <Bookshop/>
+      <QNA/>
     </div>
   );
 }
---
+++ src/Component/QNA/QNA.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QNA = () => {
+    return (
+        <div>
+            <h1>this is qna</h1>
+        </div>
+    );
+};
+
+export default QNA;
